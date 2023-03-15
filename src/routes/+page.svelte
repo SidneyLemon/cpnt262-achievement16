c167219d045b4bbfad9851a3036a967ed8527bc6
Sidney Lemon <sid.lemon3@gmail.com>
2023-03-15 13:32:09 -0600

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  let toDo = [
+    "Get out of bed",
+    "Make coffee",
+    "Read feeds",
+    "Have shower",
+    "Brush teeth",
+    "Get dressed",
+    "Leave for work",
+    "Get breakfast to go",
+  ];
+</script>
+
+<h1>Assignment 16</h1>
+<ol>
+  {#each toDo as toDo}
+    <li><strong>{toDo}</strong></li>
+  {/each}
+</ol>
